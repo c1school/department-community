04a7be0a2c1a448b9f16a28be910871d1911003e
c1cshool <nanohakase735@gmail.com>
2024-12-02 14:55:41 +0900

    Firebase 연동 및 코드 수정4

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,25 @@
 // firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyC13K__GbbC3LcuBnjpODwtnpg4qrO7KKA",
+  authDomain: "test-98eee.firebaseapp.com",
+  databaseURL: "https://test-98eee-default-rtdb.firebaseio.com",
+  projectId: "test-98eee",
+  storageBucket: "test-98eee.firebasestorage.app",
+  messagingSenderId: "106668710337",
+  appId: "1:106668710337:web:8b124772941eade4b6b740",
+  measurementId: "G-Q52VRX5X3R"
 };
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const db = getDatabase(app); // Realtime Database 초기화
+const analytics = getAnalytics(app);
+
+console.log("Firebase App Initialized:", app);
+console.log("Realtime Database Initialized:", db);
 
 export { db }; // 다른 스크립트에서 가져다 쓰기 위해 내보내기
